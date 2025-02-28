907d839c466d3a34626f504b883a302c3b8180b4
kaviyamagudeeswaran <kaviyamagudeeswaran3062005@gmail.com>
2025-02-28 10:44:18 +0530

    Delete Task-3 directory

--- Task-3/my-react-app/src/Components/StudentName.jsx
+++
@@ -1,7 +0,0 @@
-import "../styles/studentName.css"; // Import CSS file
-
-const StudentName = ({ name }) => {
-  return <div className="student-name">Student Name: {name}</div>;
-};
-
-export default StudentName;
